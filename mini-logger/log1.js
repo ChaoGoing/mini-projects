648f3aaef9f02991c8bf5e811b5ed9fbfb787a56
陈子超 <chenzichao@ydjdev.com>
2023-01-10 18:10:47 +0800

    feat: log&css

---
+++ mini-logger/log1.js
@@ -0,0 +1,16 @@
+const createlog =
+    util =>
+    (...args) => {
+        const fun = console[util] ? console[util] : console.log
+        fun.apply(void 0, args)
+    }
+const gererateColorLogStr = (...arr) => {
+    let fi = ['']
+    for (let key = 0; key < arr.length; key++) {
+        const [first, ...other] = arr[key]
+        fi[0] += `%c${first}`
+        fi = fi.concat(other)
+    }
+    return fi
+}
+createlog('log')(...gererateColorLogStr(['123', 'color: blue'], ['456', 'color: green']))
