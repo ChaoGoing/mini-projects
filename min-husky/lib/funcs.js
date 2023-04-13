2333d6796628a93a976860aeb75a3beeacb57dba
陈子超 <chenzichao@ydjdev.com>
2023-04-13 18:01:39 +0800

    chroes

--- min-husky/lib/funcs.js
+++ min-husky/lib/funcs.js
@@ -1,33 +1,36 @@
-const cp = require('child_process')
-const git = (args) => cp.spawnSync('git', args, { stdio: 'inherit' });
-const fs = require('fs');
-const p = require('path');
+const cp = require("child_process");
+const git = (args) => cp.spawnSync("git", args, { stdio: "inherit" });
+const fs = require("fs");
+const p = require("path");
 const cwd = process.cwd();
 
-exports.install = function(dir = '.minhusky') {
+exports.install = function (dir = ".minhusky") {
   // Ensure that we're not trying to install outside of cwd
   if (!p.resolve(process.cwd(), dir).startsWith(process.cwd())) {
-    throw new Error(`.. not allowed (see ${url})`)
+    throw new Error(`.. not allowed `);
   }
-  if (!fs.existsSync('.git')) {
-    throw new Error(`.git can't be found (see ${url})`)
+  if (!fs.existsSync(".git")) {
+    throw new Error(`.git can't be found `);
   }
   try {
     // Create .husky/_
-    fs.mkdirSync(p.join(dir, '_'), { recursive: true })
+    fs.mkdirSync(p.join(dir, "_"), { recursive: true });
 
     // Create .husky/_/.gitignore
-    fs.writeFileSync(p.join(dir, '_/.gitignore'), '*')
+    fs.writeFileSync(p.join(dir, "_/.gitignore"), "*");
 
     // Copy husky.sh to .husky/_/husky.sh
-    fs.copyFileSync(p.join(__dirname, '../husky.sh'), p.join(dir, '_/husky.sh'))
+    fs.copyFileSync(
+      p.join(__dirname, "../husky.sh"),
+      p.join(dir, "_/husky.sh")
+    );
 
     // Configure repo
-    const { error } = git(['config', 'core.hooksPath', dir])
+    const { error } = git(["config", "core.hooksPath", dir]);
     if (error) {
-      throw error
+      throw error;
     }
   } catch (e) {
-    throw e
+    throw e;
   }
-}
+};
