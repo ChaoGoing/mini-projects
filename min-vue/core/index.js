20c4f76b0b07744868c65973aa5516ce361a1a98
陈子超 <chenzichao@ydjdev.com>
2024-01-13 18:00:54 +0800

    feat: 20240113

--- min-vue/core/index.js
+++ min-vue/core/index.js
@@ -1,33 +1,28 @@
-import { watchEffect}  from './reactivity/index.js'
-import { mountElement, diff } from './render.js'
-import {h} from './h.js'
-
+import { watchEffect } from "./reactivity/index.js";
+import { mountElement, diff } from "./render.js";
+import { h } from "./h.js";
 
 export const createApp = (rootComponent) => {
   const app = {
     mount: (rootCotainerId) => {
-      const rootCotainer = document.querySelector(rootCotainerId)
+      const rootCotainer = document.querySelector(rootCotainerId);
       const setupResult = rootComponent.setup();
-      let isMounted = false
-      let prevTree
+      let isMounted = false;
+      let prevTree;
       watchEffect(() => {
-        
-        if(!isMounted) {
+        if (!isMounted) {
           const subTree = rootComponent.render.call(setupResult, h);
-          mountElement(subTree, rootCotainer)
-          prevTree = subTree
-          isMounted = true
-          
-        }else {
-          
+          mountElement(subTree, rootCotainer);
+          prevTree = subTree;
+          isMounted = true;
+        } else {
           const subTree = rootComponent.render.call(setupResult, h);
-          console.log('diff', prevTree, subTree)
-          diff(prevTree, subTree)
-          prevTree = subTree
+          console.log("diff", prevTree, subTree);
+          diff(prevTree, subTree);
+          prevTree = subTree;
         }
-        
-      })
-    }
-  }
-  return app
+      });
+    },
+  };
+  return app;
 };
